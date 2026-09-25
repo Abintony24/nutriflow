f2924b5a4fcb90a0a70a46708a1737a1659c722f
jijojohn911 <jijojohn911@gmail.com>
2026-09-25 14:15:33 +0530

    final push

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE_URL = 'http://localhost:8000/nutriflow'
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/nutriflow'
 
 const api = axios.create({
   baseURL: API_BASE_URL
